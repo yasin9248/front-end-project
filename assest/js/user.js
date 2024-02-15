40803058cbe2cde70539370b9abc4b4ce4c540f8
yasin <yasinhemat6@gmail.com>
2024-02-16 00:10:49 +0330

    part 13

--- assest/js/user.js
+++ assest/js/user.js
@@ -1,2 +1,15 @@
-var elems = document.querySelectorAll('.collapsible');
-var instances = M.Collapsible.init(elems, {});
+document.addEventListener('DOMContentLoaded', function() {
+          var elems = document.querySelectorAll('.collapsible');
+          var instances = M.Collapsible.init(elems, {});
+
+          const adduserbutton = document.getElementById('add_user_button')
+          adduserbutton.addEventListener('click' , ()=>{
+                    document.querySelector('.add_user_modal').classList.add('show')
+                    document.querySelector('.add_user_modal_back').classList.remove('dis-none')
+          })
+          document.querySelector('.add_user_modal_back').addEventListener('click' , (e)=>{
+                    document.querySelector('.add_user_modal').classList.remove('show') 
+                    e.target.classList.add('dis-none')    
+          })
+      });
+ 
